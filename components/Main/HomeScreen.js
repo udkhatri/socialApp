055224aaae47d5150fb3595046a9644b8d01d865
Uday <udayskhatri@gmail.com>
2022-08-04 21:36:15 -0400

    Added home screen layout

--- components/Main/HomeScreen.js
+++ components/Main/HomeScreen.js
@@ -1,12 +1,40 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect,useState } from "react";
+import { View, Text, Image, ImageBackground } from "react-native";
+import { Appbar} from "react-native-paper";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
+
   return (
-    <View>
-      <Text>HomeScreen 123</Text>
-    </View>
-  )
-}
+    <ImageBackground
+      source={require("../../assets/backface.jpg")}
+      style={{ flex: 1, resizeMode: "cover", justifyContent: "center" }}
+    >
+      <View style={{ flex: 1 }}>
+        <Appbar.Header
+          style={{
+            backgroundColor: "#fff0",
+            justifyContent: "space-between",
+            borderRadius: 15,
+            elevation: 0,
+          }}
+        >
+          <Appbar.Action
+            icon="camera-outline"
+            onPress={() => console.log("press")}
+          />
+          <Image
+            source={require("../../assets/insta.png")}
+            style={{ height: 39, width: 120 }}
+          />
+          <Appbar.Action
+            icon="message-text-outline"
+            onPress={() => console.log("press")}
+          />
+        </Appbar.Header>
+        <Text>feed screen</Text>
+      </View>
+    </ImageBackground>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
