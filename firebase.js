784f95865a78823560a54334a1a489e819f7b46d
Uday <udayskhatri@gmail.com>
2022-09-10 19:41:20 -0400

    remove firebase config keys

--- firebase.js
+++ firebase.js
@@ -1,16 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAJACoavkHZ1xzklRJ5TTB4KhseNxpcwPM",
-  authDomain: "socialshare-2f71b.firebaseapp.com",
-  projectId: "socialshare-2f71b",
-  storageBucket: "socialshare-2f71b.appspot.com",
-  messagingSenderId: "747767873576",
-  appId: "1:747767873576:web:709739659cb3dfeeb4351d",
-  measurementId: "G-9K9NRGGP69",
-};
+import { firebaseConfig } from "./components/Main/firebaseConfig";
 
 let app;
 
